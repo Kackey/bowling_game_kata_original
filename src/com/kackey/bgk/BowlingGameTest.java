8e3d75ca7f163ca7454c57e58fa17b1a449b8a74
Kackey <hide.kaki0910@gmail.com>
2026-02-01 08:50:39 +0900

    Add 1game loop for 20 rolls.

--- src/com/kackey/bgk/BowlingGameTest.java
+++ src/com/kackey/bgk/BowlingGameTest.java
@@ -7,5 +7,7 @@ public class BowlingGameTest {
     @Test
     public void testGutterGame() throws Exception {
         Game g = new Game();
+        for (int i=0; i<20; i++)
+            g.roll(0);
     }
 }
