1139b221da079fc000ffa3d120ea98d0674e62f0
Kackey <hide.kaki0910@gmail.com>
2026-01-31 17:32:31 +0900

    Test-01 Green

---
+++ src/com/kackey/bgk/BowlingGameTest.java
@@ -0,0 +1,11 @@
+package com.kackey.bgk;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BowlingGameTest {
+    @Test
+    public void testGutterGame() throws Exception {
+        Game g = new Game();
+    }
+}
